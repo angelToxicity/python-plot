484eddd77a7e4d5e78a6bc1b3b63901f2ca363b4
Angel Rodriguez <arodriguez@payall.com.ve>
2020-01-10 12:33:26 -0400

    Optimizar funciones de plot
    Se optimizan las lineas de codigo para realizar el plot de las funciones, compactando todo en una sola funcion

--- plot_3.py
+++ plot_3.py
@@ -6,6 +6,26 @@ import math
 
 mplstyle.use(['dark_background', 'ggplot'])
 
+def plot_function(x,y,type):
+    if type == 'linear':
+        f = []
+        g = []
+        for i in y:
+            f.append(i)
+            if len(x) == 2:
+                g.append((x[0]*i)+x[1])
+            elif len(x) == 3:
+                g.append((x[0]*i*i)+(x[1]*i)+x[2])
+            elif len(x) == 4:
+                g.append((x[0]*i*i*i)+(x[1]*i*i)+(x[2]*i)+x[3])
+            elif len(x) == 5:
+                g.append((x[0]*i*i*i*i)+(x[1]*i*i*i)+(x[2]*i*i)+(x[3]*i)+x[4])
+        plot.plot(f,g)
+    else:
+        plot.plot(x,y)
+
+    plot.show()
+
 def is_number(user_input):
     try:
         val = int(user_input)
@@ -63,13 +83,7 @@ def graph_line():
     b = input()
     b = is_float(b)
     c = np.arange(y,z)
-    f = []
-    g = []
-    for i in c:
-        f.append(i)
-        g.append((a*i)+b)
-    plot.plot(f,g)
-    plot.show()
+    plot_function([a,b],c,'linear')
 
 def graph_parab():
     print("Ingrese el rango inicial de la funcion a graficar:")
@@ -88,13 +102,7 @@ def graph_parab():
     d = input()
     d = is_float(d)
     c = np.arange(y,z)
-    f = []
-    g = []
-    for i in c:
-        f.append(i)
-        g.append((a*i*i)+(b*i)+d)
-    plot.plot(f,g)
-    plot.show()
+    plot_function([a,b,d],c,'linear')
 
 def graph_cubic():
     print("Ingrese el rango inicial de la funcion a graficar:")
@@ -116,13 +124,7 @@ def graph_cubic():
     e = input()
     e = is_float(e)
     c = np.arange(y,z)
-    f = []
-    g = []
-    for i in c:
-        f.append(i)
-        g.append((a*i*i*i)+(b*i*i)+(d*i)+e)
-    plot.plot(f,g)
-    plot.show()
+    plot_function([a,b,d,e],c,'linear')
 
 def graph_cuadratic():
     print("Ingrese el rango inicial de la funcion a graficar:")
@@ -147,13 +149,7 @@ def graph_cuadratic():
     h = input()
     h = is_float(h)
     c = np.arange(y,z)
-    f = []
-    g = []
-    for i in c:
-        f.append(i)
-        g.append((a*i*i*i*i)+(b*i*i*i)+(d*i*i)+(e*i)+h)
-    plot.plot(f,g)
-    plot.show()
+    plot_function([a,b,d,e,h],c,'linear')
 
 def graph_sin():
     print("Ingrese el rango inicial de la funcion a graficar:")
@@ -167,8 +163,7 @@ def graph_sin():
     a = is_float(a)
     c = np.arange(y,z,0.2)
     g = a*np.sin(c)
-    plot.plot(c,g)
-    plot.show()
+    plot_function(c,g,'sin')
 
 def graph_sin_t():
     print("Ingrese el rango inicial de la funcion a graficar:")
@@ -182,9 +177,7 @@ def graph_sin_t():
     a = is_float(a)
     c = np.arange(y,z,0.2)
     g = (a*np.sin(c))/c
-    plot.plot(c,g)
-    plot.show()
-
+    plot_function(c,g,'sin')
 
 def select_graph(i):
     if i == 1:
@@ -208,7 +201,6 @@ def main():
     value = is_number(val)
     if (value > 0 and value < 8):
         select_graph(value)
-
 
 if __name__ == "__main__":
     main()
